00819e4e027449db73444a09d893d3650bd70aa4
Dane Urban <danelegend13@gmail.com>
2025-12-18 15:55:03 +1100

    .

--- bot/channel.py
+++ bot/channel.py
@@ -44,7 +44,7 @@ class Channel(DataConsumer):
         for asset in strategy.get_asset_ids():
             asset_id = asset.asset_id
             self.asset_strategy_map[asset_id].append(strategy_id)
-            self.add_asset(asset_id)
+            self.add_asset(str(asset_id))
 
     def add_asset(self, asset_id: str):
         self.orderbook_manager.create_order_book(asset_id)
