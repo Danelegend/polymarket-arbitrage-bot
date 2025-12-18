7ff959beb18160ba95dc397ec2d624b820c00a85
Dane Urban <danelegend13@gmail.com>
2025-12-18 14:16:50 +1100

    Remove dead code

--- bot/orderbook/__init__.py
+++ bot/orderbook/__init__.py
@@ -1,10 +1,7 @@
-from .orderbook_fanout_interface import OrderBookConsumer, OrderBookFanoutInterface
 from .orderbook import OrderBook
 from .orderbook_manager import OrderManager
 
 __all__ = [
-    'OrderBookConsumer',
-    'OrderBookFanoutInterface',
     'OrderBook',
     'OrderManager'
 ]
