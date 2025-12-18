d1d4e521f348db7ea8f5c0822a5764b186a8fa00
Dane Urban <danelegend13@gmail.com>
2025-12-18 13:18:07 +1100

    parse properly

--- bot/ids/ids_reader.py
+++ bot/ids/ids_reader.py
@@ -1,3 +1,4 @@
+import json
 from typing import Generator
 
 from bot.common.types.ids import MarketInformation, TradeableMarket
@@ -6,8 +7,10 @@ from .constants import MARKET_OUTPUT_FILE, TRADABLE_MARKETS_OUTPUT_FILE
 def read_markets() -> Generator[MarketInformation, None, None]:
     with open(MARKET_OUTPUT_FILE, 'r') as f:
         for line in f:
-            yield MarketInformation(**line) 
+            if not line.strip():
+                continue
+            yield MarketInformation(**json.loads(line))
 
 def read_tradeable_markets() -> list[TradeableMarket]:
     with open(TRADABLE_MARKETS_OUTPUT_FILE, 'r') as f:
-        return [TradeableMarket(**line) for line in f]
+        return [TradeableMarket(**json.loads(line)) for line in f if line.strip()]
